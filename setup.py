4c0e454d3f9cc351e3ec7af6e553dfc0a836c921
smithumble <smithumble@gmail.com>
2021-05-07 02:57:20 +0300

    Fix requirements in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ with open('requirements.txt', 'r') as f:
     for resource in f.readlines():
         if not resource.startswith('git+'):
             requirements.append(resource.strip())
+        else:
+            res = resource.strip()
+            egg = res.split("#egg=")[1]
+            requires.append("@".join([egg, res]))
 
 setup(
     name='prozorro_auction',
